84ad9a060ccafd9acab211cc7edd68d2ad8e1cc1
Leyla Shafiyeva <llshafi03@gmail.com>
2022-11-15 21:40:29 +0400

    Add project files.

---
+++ DesignPatternsHW_3/Program.cs
@@ -0,0 +1,46 @@
+﻿
+public interface IBuilder
+{
+    void reset();
+    void setSeats(int number);
+    void setEngine(int engine);
+    void setTripComputer();
+    void setGPS();
+}
+
+public class CarBuilder : IBuilder
+{
+    public void reset()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void setEngine(int engine)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void setGPS()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void setSeats(int number)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void setTripComputer()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+
+    }
+}
---
+++ DesignPattrnsHW_1/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace FactoryMethod;
+
+public interface IButton
+{
+    void render();
+    void OnClick();
+}
+
+public class WindowsButton : IButton
+{
+    public void OnClick() => Console.WriteLine("Windows button Click");
+    public void render() => Console.WriteLine("Windows button Render");
+}
+
+public class HtmlButton : IButton
+{
+    public void OnClick() => Console.WriteLine("HTML button Click");
+    public void render() => Console.WriteLine("HTML render Render");
+}
+
+
+public abstract class Dialog
+{
+    public void render()
+    {
+        IButton okButton = CreateButton();
+        okButton.OnClick();
+        okButton.render();
+    }
+
+    public abstract IButton CreateButton();
+}
+
+public class WindowsDialog : Dialog
+{
+    public override IButton CreateButton() => new WindowsButton();
+}
+
+public class HtmlDialog : Dialog
+{
+    public override IButton CreateButton() => new HtmlButton();
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Dialog dialog = new WindowsDialog();
+        dialog.render();
+
+        dialog = new HtmlDialog();
+        dialog.render();
+    }
+}
